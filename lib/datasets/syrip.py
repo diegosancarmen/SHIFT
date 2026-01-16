ab5343d0040765b2a11ec500e8c95acebdf528ba
diegosancarmen <101142209+diegosancarmen@users.noreply.github.com>
2026-01-16 10:14:40 -0800

    Update syrip.py

--- lib/datasets/syrip.py
+++ lib/datasets/syrip.py
@@ -41,14 +41,11 @@ class SyRIP(Body16KeypointDataset):
         # Load data
         self.samples = []
         data = np.load('/path/to/InfantUDA/SHIFT/lib/datasets/infant_annotations/SyRIP_all.npy', allow_pickle=True).item()
-        # data = np.load('/home/coeguest/hdelacruz/DAIP/Experiments_2024/102024/SHIFT/prior/RePoGen/RePoGen.npy', allow_pickle=True).item()
         data = data[split]
         for _, item in enumerate(tqdm(data.keys())):
             if split in ['train', 'prior']:
                 image_root = "/path/to/SyRIP/data/syrip/images/train_infant"
-                # image_root = "/home/coeguest/hdelacruz/DAIP/Experiments_2024/102024/RePoGen/RePoGen/top_and_bottom"
                 img_path = os.path.join(image_root, f"train{item:05}.jpg")
-                # img_path = os.path.join(image_root, f"{item}.jpg")
 
             elif split == "validate":
                 image_root = "/path/to/SyRIP/data/syrip/images/validate_infant"
